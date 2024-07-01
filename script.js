a9c8f2f2b236d57e44b5fbf2a0ff78094db09f7a
Yiim <ilyimyan@gmail.com>
2024-07-01 12:38:08 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiKey = 'ba552efbe4a226aadcc20b0fa29f7878';
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=samarinda&appid=&units=metric';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?=&units=metric';
 
 
 
