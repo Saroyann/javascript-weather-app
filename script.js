9d1f9abe272fce8de22a591631dfb0186d910c74
Yiim <ilyimyan@gmail.com>
2024-07-01 12:56:45 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const apiKey = 'ba552efbe4a226aadcc20b0fa29f7878';
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?=&units=metric&q=samarinda';
 
@@ -7,6 +9,16 @@ const checkWeather = async () => {
     let data = await response.json();
 
     console.log(data);
+
+    document.querySelector('.city').innerHTML = data.name;
+
+    document.querySelector('.temp').innerHTML = data.main.temp + `°c`;
+
+    document.querySelector('.humidity').innerHTML = data.main.humidity + `%`;
+
+    document.querySelector('.wind').innerHTML = data.wind.speed + ` km/jam`;
+
+
 }
 
 checkWeather();
