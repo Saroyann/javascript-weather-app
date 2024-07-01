2cf5b8c7d87414a64409eecad9b6de556d96c7b4
Yiim <ilyimyan@gmail.com>
2024-07-01 13:56:00 +0800

    initial commit

--- script.js
+++ script.js
@@ -2,48 +2,43 @@ const apiKey = 'ba552efbe4a226aadcc20b0fa29f7878';
 const apiBaseURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const weatherIcon = document.querySelector('.weather-icon');
-
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
+
+const getWeatherIcon = (weather) => {
+    const iconMap = {
+        'Clouds': 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/cloudy.png?raw=true',
+        'Rain': 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/rain.png?raw=true',
+        'Clear': 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/sunny.png?raw=true',
+        'Haze': 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/mist.png?raw=true',
+        'Drizzle': 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/drizzle.png?raw=true'
+    };
+    return iconMap[weather] || '';
+};
+
+const updateWeatherInfo = (data) => {
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = `${Math.round(data.main.temp)}°C`;
+    document.querySelector('.humidity').innerHTML = `${data.main.humidity}%`;
+    document.querySelector('.wind').innerHTML = `${data.wind.speed} km/jam`;
+    weatherIcon.src = getWeatherIcon(data.weather[0].main);
+};
 
 const checkWeather = async (city) => {
     const apiURL = `${apiBaseURL}?q=${city}&units=metric&appid=${apiKey}`;
     try {
         const response = await fetch(apiURL);
-
         if (!response.ok) {
             throw new Error(`Error: ${response.statusText}`);
         }
-
         const data = await response.json();
         console.log(data);
-
-        document.querySelector('.city').innerHTML = data.name;
-        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + `°C`;
-        document.querySelector('.humidity').innerHTML = data.main.humidity + `%`;
-        document.querySelector('.wind').innerHTML = data.wind.speed + ` km/jam`;
-
-        if(data.weather[0].main == 'Clouds') {
-            weatherIcon.src = 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/cloudy.png?raw=true';
-
-        } else if(data.weather[0].main == 'Rain') {
-            weatherIcon.src = 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/rain.png?raw=true';
-
-        } else if(data.weather[0].main == 'Clear') {
-            weatherIcon.src = 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/sunny.png?raw=true';
-
-        } else if(data.weather[0].main == 'Haze') {
-            weatherIcon.src = 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/mist.png?raw=true';
-
-        } else if (data.weather[0].main == 'Drizzle') {
-            weatherIcon.src = 'https://github.com/Saroyann/javascript-weather-app/blob/master/img/drizzle.png?raw=true';
-        }
-
+        updateWeatherInfo(data);
     } catch (error) {
         console.error(error);
         alert('Gagal mendapatkan data cuaca. Silakan coba lagi.');
     }
-}
+};
 
 searchBtn.addEventListener('click', () => {
     const city = searchBox.value.trim();
