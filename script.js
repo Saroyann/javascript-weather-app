7fe18735b046c535f95f00d1f8d858cfcfb37290
Yiim <ilyimyan@gmail.com>
2024-07-01 13:10:40 +0800

    initial commit

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 const apiKey = 'ba552efbe4a226aadcc20b0fa29f7878';
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?=&units=metric&q=samarinda';
 
-const checkWeather = async () => {
-    const response = await fetch(apiURL + `&appid=${apiKey}`);
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+
+const checkWeather = async (city) => {
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
 
     let data = await response.json();
 
@@ -12,7 +15,7 @@ const checkWeather = async () => {
 
     document.querySelector('.city').innerHTML = data.name;
 
-    document.querySelector('.temp').innerHTML = data.main.temp + `°c`;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp)  + `°c`;
 
     document.querySelector('.humidity').innerHTML = data.main.humidity + `%`;
 
@@ -21,6 +24,10 @@ const checkWeather = async () => {
 
 }
 
-checkWeather();
+searchBtn.addEventListener('click', () => {
+    checkWeather(searchBox.value);
+})
+
+
 
 
